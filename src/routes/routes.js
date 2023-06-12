53f0d066bca0060bf31f8fa619fa61fb532c71b8
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-12 14:13:21 +0530

    feat: Mockman Setup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,15 +8,19 @@ import RootLayout from "../layout/RootLayout";
 import ExplorePage from "../pages/explore/ExplorePage";
 import BookmarksPage from "../pages/bookmarks/BookmarksPage";
 import ProfilePage from "../pages/profile/ProfilePage";
+import MockmanPage from "../pages/MockmanPage";
 
 const routes = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="explore" element={<ExplorePage />} />
-      <Route path="bookmarks" element={<BookmarksPage />} />
-      <Route path="profile" element={<ProfilePage />} />
-    </Route>
+    <>
+      <Route path="/mockman" element={<MockmanPage />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="explore" element={<ExplorePage />} />
+        <Route path="bookmarks" element={<BookmarksPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+      </Route>
+    </>
   )
 );
 
