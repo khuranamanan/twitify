143ea49440720b3ca4a7dcda8a37ab7055c0eddb
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-14 17:06:06 +0530

    style: Widgets Component

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -1,5 +1,9 @@
 function Widgets() {
-  return <div>Widgets</div>;
+  return (
+    <div className="bg-black w-[20rem] border-l border-darkerGray h-full">
+      Widgets
+    </div>
+  );
 }
 
 export default Widgets;
