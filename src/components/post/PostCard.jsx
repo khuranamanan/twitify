cdb6db13381e56c0dd3c3c3831a4a2a2b1ef62bf
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-19 21:46:41 +0530

    style: Post Card

--- src/components/post/PostCard.jsx
+++ src/components/post/PostCard.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProfileImage from "../ProfileImage";
 
 function PostCard({ postData }) {
+  const { allUsers } = useSelector((state) => state.allUsers);
+
+  const currentPostUser = allUsers.find(
+    (twitifyUser) => twitifyUser.username === postData.username
+  );
+
+  const date = postData.createdAt;
+
   return (
-    <div className="p-4 border-y border-solid border-darkerGray">
-      <p>{`@${postData.username}`}</p>
-      <p>{postData.content}</p>
+    <div className="p-4 border-y border-solid border-darkerGray grid grid-cols-[auto_auto_1fr] grid-rows-[auto_auto]  gap-x-4 gap-y-2 justify-start items-center">
+      <div className="col-start-1 col-end-2 w-fit">
+        <ProfileImage
+          userImage={currentPostUser.profileImg}
+          userFirstName={currentPostUser.firstName}
+        />
+      </div>
+
+      <div className="row-start-1 row-end-2 col-start-3 col-end-4 self-start text-xs text-darkGray mt-2">
+        {date}
+      </div>
+
+      <div className="flex flex-col col-start-2 col-end-3 row-start-1 row-end-2">
+        <p className="text-lg font-bold">{`${currentPostUser.firstName} ${currentPostUser.lastName}`}</p>
+        <p className="text-sm text-darkGray">{`@${postData.username}`}</p>
+      </div>
+
+      <div className="col-start-2 col-end-4 row-start-2 row-end-3">
+        <p>{postData.content}</p>
+      </div>
     </div>
   );
 }
--- src/pages/explore/ExplorePage.jsx
+++ src/pages/explore/ExplorePage.jsx
@@ -1,16 +1,19 @@
 import { useSelector } from "react-redux";
 import PostCard from "../../components/post/PostCard";
+import { Fragment } from "react";
 
 function ExplorePage() {
   const { allPosts } = useSelector((state) => state.posts);
 
   return (
-    <div>
+    <div className="mb-[64px] sm:mb-4">
       <div className="bg-black px-4 py-3 border-b border-solid border-darkerGray text-3xl font-bold">
         <h1>Explore</h1>
       </div>
       {allPosts.map((post) => (
-        <PostCard postData={post} />
+        <Fragment key={post._id}>
+          <PostCard postData={post} />
+        </Fragment>
       ))}
     </div>
   );
