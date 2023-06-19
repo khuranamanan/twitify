7721fcadc688fa891c34dca21c5e8e1a0660bceb
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-19 22:20:26 +0530

    Started Working on HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,5 +1,14 @@
+import { TwitifyLogoIconSmall } from "../../assets/icons";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="mb-[64px] sm:mb-4">
+      <div className="sm:hidden bg-black px-4 pt-4 pb-5 flex justify-center border-b border-solid border-darkerGray h-fit">
+        <TwitifyLogoIconSmall />
+      </div>
+      HomePage
+    </div>
+  );
 }
 
 export default HomePage;
