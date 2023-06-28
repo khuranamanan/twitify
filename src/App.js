9a82b2676379cdf36815cc1d2ce6be0c4974dd3d
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-28 17:49:52 +0530

    refactor: load posts, users on Log In

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { RouterProvider } from "react-router";
 import routes from "./routes/routes";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "./redux/slices/allUsersSlice";
 import { getAllPosts } from "./redux/slices/postsSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    dispatch(getAllUsers());
-    dispatch(getAllPosts());
-  }, [dispatch]);
+    if (isLoggedIn) {
+      dispatch(getAllUsers());
+      dispatch(getAllPosts());
+    }
+  }, [dispatch, isLoggedIn]);
 
   return (
     <div className="bg-black h-dvh-screen text-white">
