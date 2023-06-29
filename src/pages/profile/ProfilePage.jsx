2b09be72fc4469a5579dbd41cf852e190dea4f2b
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-29 17:00:11 +0530

    refactor: Split code of Profile Page into 2

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -2,25 +2,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import { useEffect } from "react";
-import {
-  followAUser,
-  getProfilePageUser,
-  unfollowAUser,
-} from "../../redux/slices/allUsersSlice";
+import { getProfilePageUser } from "../../redux/slices/allUsersSlice";
 import { getProfilePageUserPosts } from "../../redux/slices/postsSlice";
 import { BeatLoader } from "react-spinners";
 import defaultCoverImg from "../../assets/defaultCoverImg.jpg";
-import defaultProfileImg from "../../assets/defaultProfileImg.png";
-import { ArrowLeftIcon, UrlIcon } from "../../assets/icons";
+import { ArrowLeftIcon } from "../../assets/icons";
 import PostSkeleton from "../../components/post/PostSkeleton";
 import PostCard from "../../components/post/PostCard";
 import FlipMove from "react-flip-move";
+import UserHeader from "./UserHeader";
 
 function ProfilePage() {
   const { username } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { user, token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth);
   const { profilePageUser, profilePageUserStatus, allUsers } = useSelector(
     (state) => state.allUsers
   );
@@ -42,97 +38,10 @@ function ProfilePage() {
     dispatch(getProfilePageUserPosts(username));
   }, [username, dispatch, allPosts]);
 
-  function handleFollow() {
-    dispatch(followAUser({ followUserID: profilePageUser._id, token: token }));
-  }
-
-  function handleUnfollow() {
-    dispatch(
-      unfollowAUser({ followUserID: profilePageUser._id, token: token })
-    );
-  }
-
-  function renderCTAButton() {
-    if (isThisUsersProfilePage) {
-      return (
-        <button className="text-lg font-semibold rounded-full px-4 py-1 text-white border border-darkerGray hover:opacity-90">
-          Edit Profile
-        </button>
-      );
-    } else if (isUserFollowingThisUser) {
-      return (
-        <button
-          className="text-lg font-semibold rounded-full px-4 py-1 text-white border border-darkerGray hover:opacity-90"
-          onClick={handleUnfollow}
-        >
-          Unfollow
-        </button>
-      );
-    } else {
-      return (
-        <button
-          className="bg-snow text-lg font-semibold rounded-full px-4 py-1 text-black border border-snow hover:opacity-90"
-          onClick={handleFollow}
-        >
-          Follow
-        </button>
-      );
-    }
-  }
-
   function renderLoader() {
     return (
       <div className="flex justify-center items-center border-b border-darkerGray p-4 h-[60vh]">
         <BeatLoader color="#3fc1c9" />
-      </div>
-    );
-  }
-
-  function renderUserDetails() {
-    return (
-      <div className="flex flex-col gap-2 justify-center p-4 border-b border-darkerGray">
-        <div className="relative flex items-center justify-end gap-4">
-          <div className="mb-3 absolute bottom-0 left-0 w-24 h-24">
-            <img
-              src={profilePageUser.profileImg || defaultProfileImg}
-              alt={profilePageUser.firstName}
-              className="bg-white border-4 border-black rounded-full object-cover"
-            />
-          </div>
-          {renderCTAButton()}
-        </div>
-        <div className="font-inter">
-          <h2 className="text-2xl font-semibold">
-            {profilePageUser.firstName} {profilePageUser.lastName}
-          </h2>
-          <p className="text-gray">@{profilePageUser.username}</p>
-          {profilePageUser.profileBio && (
-            <p className="text-gray mt-2">{profilePageUser.profileBio}</p>
-          )}
-          {profilePageUser.profileURL && (
-            <p className="flex items-center gap-1 mt-2">
-              <UrlIcon />{" "}
-              <a
-                href={profilePageUser.profileURL}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                {profilePageUser.profileURL}
-              </a>
-            </p>
-          )}
-        </div>
-        <div className="text-sm flex gap-2">
-          <p>
-            {profilePageUser.following.length}{" "}
-            <span className="text-darkGray">Following</span>
-          </p>
-          <p>
-            {profilePageUser.followers.length}{" "}
-            <span className="text-darkGray">Followers</span>
-          </p>
-        </div>
       </div>
     );
   }
@@ -192,7 +101,11 @@ function ProfilePage() {
           </div>
 
           {/* ProfilePageUser Details */}
-          {renderUserDetails()}
+          <UserHeader
+            profilePageUser={profilePageUser}
+            isThisUsersProfilePage={isThisUsersProfilePage}
+            isUserFollowingThisUser={isUserFollowingThisUser}
+          />
         </div>
       )}
 
---
+++ src/pages/profile/UserHeader.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { UrlIcon } from "../../assets/icons";
+import defaultProfileImg from "../../assets/defaultProfileImg.png";
+import { useDispatch, useSelector } from "react-redux";
+import { followAUser, unfollowAUser } from "../../redux/slices/allUsersSlice";
+
+function UserHeader({
+  profilePageUser,
+  isThisUsersProfilePage,
+  isUserFollowingThisUser,
+}) {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+
+  function handleFollow() {
+    dispatch(followAUser({ followUserID: profilePageUser._id, token: token }));
+  }
+
+  function handleUnfollow() {
+    dispatch(
+      unfollowAUser({ followUserID: profilePageUser._id, token: token })
+    );
+  }
+
+  function renderCTAButton() {
+    if (isThisUsersProfilePage) {
+      return (
+        <button className="text-lg font-semibold rounded-full px-4 py-1 text-white border border-darkerGray hover:opacity-90">
+          Edit Profile
+        </button>
+      );
+    } else if (isUserFollowingThisUser) {
+      return (
+        <button
+          className="text-lg font-semibold rounded-full px-4 py-1 text-white border border-darkerGray hover:opacity-90"
+          onClick={handleUnfollow}
+        >
+          Unfollow
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className="bg-snow text-lg font-semibold rounded-full px-4 py-1 text-black border border-snow hover:opacity-90"
+          onClick={handleFollow}
+        >
+          Follow
+        </button>
+      );
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-2 justify-center p-4 border-b border-darkerGray">
+      <div className="relative flex items-center justify-end gap-4">
+        <div className="mb-3 absolute bottom-0 left-0 w-24 h-24">
+          <img
+            src={profilePageUser.profileImg || defaultProfileImg}
+            alt={profilePageUser.firstName}
+            className="bg-white border-4 border-black rounded-full object-cover"
+          />
+        </div>
+        {renderCTAButton()}
+      </div>
+      <div className="font-inter">
+        <h2 className="text-2xl font-semibold">
+          {profilePageUser.firstName} {profilePageUser.lastName}
+        </h2>
+        <p className="text-gray">@{profilePageUser.username}</p>
+        {profilePageUser.profileBio && (
+          <p className="text-gray mt-2">{profilePageUser.profileBio}</p>
+        )}
+        {profilePageUser.profileURL && (
+          <p className="flex items-center gap-1 mt-2">
+            <UrlIcon />{" "}
+            <a
+              href={profilePageUser.profileURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              {profilePageUser.profileURL}
+            </a>
+          </p>
+        )}
+      </div>
+      <div className="text-sm flex gap-2">
+        <p>
+          {profilePageUser.following.length}{" "}
+          <span className="text-darkGray">Following</span>
+        </p>
+        <p>
+          {profilePageUser.followers.length}{" "}
+          <span className="text-darkGray">Followers</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default UserHeader;
