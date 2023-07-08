0ece9bc729f8bef77ea902120ea1d244c3a2daa1
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-07-08 14:00:38 +0530

    Update posts.js

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -1,6 +1,3 @@
-import { v4 as uuid } from "uuid";
-import { formatDate } from "../utils/authUtils";
-
 /**
  * Posts can be added here.
  * You can add default posts of your wish with different attributes
@@ -8,36 +5,63 @@ import { formatDate } from "../utils/authUtils";
 
 export const posts = [
   {
-    _id: uuid(),
+    _id: "d4dc12c0-1b9a-4672-9688-b98c7f7cfb01",
     content: "The only way to do great work is to love what you do.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "adarshbalika",
     createdAt: "2023-06-20T01:30:10+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:44:16+05:30",
   },
   {
-    _id: uuid(),
+    _id: "2246cee8-76c9-4072-bfe2-babefa10da80",
     content: "Believe you can and you're halfway there.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "manankhurana",
     createdAt: "2023-03-18T16:45:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:40+05:30",
   },
   {
-    _id: uuid(),
+    _id: "66a612d7-7d3c-4552-b03e-7b46769044ad",
     content:
       "Excited to be on this web development journey with React! Learning new concepts, building projects, and expanding my skills. #WebDevLife",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     media: {
@@ -46,101 +70,205 @@ export const posts = [
     },
     username: "manankhurana",
     createdAt: "2023-05-01T22:30:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:33+05:30",
   },
   {
-    _id: uuid(),
+    _id: "3792b1e8-9ddc-4a94-a294-b3bbd7816859",
     content:
       "The pursuit of wealth without the pursuit of wisdom is a recipe for disaster.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "navalravikant",
     createdAt: "2023-01-20T05:10:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:51+05:30",
   },
   {
-    _id: uuid(),
+    _id: "47ec79fe-d91f-4308-80e5-24a4e06d8595",
     content:
       "Society is divided into two groups: those who take responsibility for their lives and those who expect someone else to take care of them.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
-      dislikedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
+      dislikedBy: [
+        {
+          _id: "t103",
+          firstName: "Naval",
+          lastName: "Ravikant",
+          username: "navalravikant",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
     },
     username: "navalravikant",
     createdAt: "2023-04-08T11:45:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:46:40+05:30",
   },
   {
-    _id: uuid(),
+    _id: "45a08ba4-2184-4062-8e1d-4c65e73250ce",
     content:
       "The most important skill to acquire is the ability to learn on your own.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "navalravikant",
     createdAt: "2023-03-06T19:20:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:45+05:30",
   },
   {
-    _id: uuid(),
+    _id: "ffdc86a0-a20e-4e34-941f-7f6f09d49679",
     content:
       "Happiness is not a destination; it's a state of mind. Cultivate gratitude and enjoy the journey.",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "navalravikant",
     createdAt: "2023-05-16T00:55:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:29+05:30",
   },
   {
-    _id: uuid(),
+    _id: "5dbb5f0d-b95c-40a3-8cc2-4d672a4551de",
     content:
       "Don't dwell on your failures. Learn from them and use them as stepping stones to success. #KeepMovingForward",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 2,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+        {
+          _id: "t103",
+          firstName: "Naval",
+          lastName: "Ravikant",
+          username: "navalravikant",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "garyvee",
     createdAt: "2023-02-28T06:30:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:46:47+05:30",
   },
   {
-    _id: uuid(),
+    _id: "b11b17d3-e405-4760-929c-951a522a7f3d",
     content:
       "Success is not guaranteed, but regret is. Take risks, embrace failure, and never wonder 'what if?' #NoRegrets",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 3,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+        {
+          _id: "t103",
+          firstName: "Naval",
+          lastName: "Ravikant",
+          username: "navalravikant",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "garyvee",
     createdAt: "2023-05-06T13:00:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:46:31+05:30",
   },
   {
-    _id: uuid(),
+    _id: "13b67912-2871-4751-b0e1-44765d70328c",
     content:
       "Hustle and patience are the keys to success. Put in the work every day and trust the process. #CrushIt",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 2,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+        {
+          _id: "t101",
+          firstName: "Adarsh",
+          lastName: "Balika",
+          username: "adarshbalika",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "garyvee",
     createdAt: "2023-03-26T19:35:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:45:39+05:30",
   },
   {
-    _id: uuid(),
+    _id: "8dbff609-cc18-4541-a53f-a27001d07b37",
     content:
       "Empathy is at the heart of innovation. By understanding the needs of others, we can create meaningful solutions. #InclusiveTech",
     likes: {
@@ -150,23 +278,32 @@ export const posts = [
     },
     username: "satyanadella",
     createdAt: "2023-04-21T02:10:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "40eb6ee6-ea03-4645-9fe6-5921a13deb65",
     content:
       "Innovation is not just about creating new products, but also about improving the lives of our customers. #AppleLife",
     likes: {
-      likeCount: 0,
-      likedBy: [],
+      likeCount: 1,
+      likedBy: [
+        {
+          _id: "t102",
+          firstName: "Manan",
+          lastName: "Khurana",
+          username: "manankhurana",
+          createdAt: "2023-07-08T13:43:57+05:30",
+          updatedAt: "2023-07-08T13:43:57+05:30",
+        },
+      ],
       dislikedBy: [],
     },
     username: "timcook",
     createdAt: "2023-01-08T08:50:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:44:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "bf901b3d-950f-4fff-92b9-1bf3c22faf4e",
     content:
       "Dream big, think long-term, and embrace failure. The path to success is paved with perseverance. #BlueOrigin",
     likes: {
@@ -176,10 +313,10 @@ export const posts = [
     },
     username: "jeffbezos",
     createdAt: "2023-03-30T15:25:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "fcc7fa24-3cdb-4133-a928-2ffc32cb9635",
     content:
       "Technology has the power to change the world. Let's build a future where everyone has access to knowledge and opportunity. #GoogleForAll",
     likes: {
@@ -189,10 +326,10 @@ export const posts = [
     },
     username: "sundarpichai",
     createdAt: "2023-04-16T22:00:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "7c12f936-b4db-452b-acc9-8e0b0e64c3ff",
     content:
       "Connecting people and building communities is at the core of our mission. Together, we can make a difference. #SocialImpact",
     likes: {
@@ -202,10 +339,10 @@ export const posts = [
     },
     username: "markzuckerberg",
     createdAt: "2023-01-24T04:35:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "d04a19d5-2b5a-4955-92d3-9f2fac0805cc",
     content:
       "Philanthropy is not just about giving back; it's about making a lasting impact and improving lives. #GivingBack",
     likes: {
@@ -215,10 +352,10 @@ export const posts = [
     },
     username: "billgates",
     createdAt: "2023-02-20T11:10:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "adcad466-551f-4dd6-8478-e494775d7f95",
     content:
       "Using humor to shed light on important issues. Laughter has the power to bring us together. #ComedyMatters",
     likes: {
@@ -228,10 +365,10 @@ export const posts = [
     },
     username: "trevornoah",
     createdAt: "2023-01-01T17:45:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
   {
-    _id: uuid(),
+    _id: "a4e37b99-c651-45c3-9262-1b28e220dd26",
     content:
       "Building and scaling innovative financial solutions. Let's empower individuals and businesses for a better tomorrow. #FintechRevolution",
     likes: {
@@ -241,6 +378,6 @@ export const posts = [
     },
     username: "kunalshah",
     createdAt: "2023-03-12T00:20:00+05:30",
-    updatedAt: formatDate(),
+    updatedAt: "2023-07-08T13:43:57+05:30",
   },
 ];
