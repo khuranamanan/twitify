7bad1396ba836e7e962f02b65b9afd1688dc0bd7
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-07-09 17:43:32 +0530

    fix: If suggestion list is 0 it will disappear

--- src/components/widgets/whoToFollow/WhoToFollow.jsx
+++ src/components/widgets/whoToFollow/WhoToFollow.jsx
@@ -16,17 +16,19 @@ function WhoToFollow() {
     );
 
   return (
-    <div className="py-4 bg-transparentBlack2 dark:bg-transparentWhite max-h-[50%] rounded-3xl flex flex-col overflow-hidden">
-      <h2 className="px-4 font-bold text-xl mb-4">Who to Follow</h2>
-      <div className="flex flex-col gap-1 grow overflow-y-scroll no-scrollbar">
-        {followSuggestionList.map((twitifyUser) => (
-          <FollowSuggestionCard
-            twitifyUser={twitifyUser}
-            key={twitifyUser.id}
-          />
-        ))}
+    followSuggestionList.length > 0 && (
+      <div className="py-4 bg-transparentBlack2 dark:bg-transparentWhite max-h-[50%] rounded-3xl flex flex-col overflow-hidden">
+        <h2 className="px-4 font-bold text-xl mb-4">Who to Follow</h2>
+        <div className="flex flex-col gap-1 grow overflow-y-scroll no-scrollbar">
+          {followSuggestionList.map((twitifyUser) => (
+            <FollowSuggestionCard
+              twitifyUser={twitifyUser}
+              key={twitifyUser.id}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 }
 
